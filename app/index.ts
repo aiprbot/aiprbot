d777e19e06fae2e05171c48e885339a18a950229
sam <aquaticcalf@proton.me>
2025-10-26 21:03:18 +0530

    final try

--- app/index.ts
+++ app/index.ts
@@ -1,7 +1,6 @@
 import { Elysia } from "elysia"
 import githubhandlers from "@/github/handlers"
 
-
 export default new Elysia()
 	.get("/", () => "hello elysia")
 	.post("/webhooks/github", githubhandlers.handlewebhook)
