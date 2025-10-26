31dcb8e3d0249f7f7aaafadf4124088253ea71b3
sam <aquaticcalf@proton.me>
2025-10-26 20:58:53 +0530

    export default

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,7 @@
 import { Elysia } from "elysia"
 import githubhandlers from "@/github/handlers"
 
-const app = new Elysia()
+
+export default new Elysia()
 	.get("/", () => "hello elysia")
 	.post("/webhooks/github", githubhandlers.handlewebhook)
-	.listen(4321)
-
-console.log(
-	`🦊 elysia is running at ${app.server?.hostname}:${app.server?.port}`,
-)
