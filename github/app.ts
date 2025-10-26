581239a1dda11252d5cdc461230480b6ffa6ca10
sam <aquaticcalf@proton.me>
2025-10-26 17:16:00 +0530

    biome

--- github/app.ts
+++ github/app.ts
@@ -1,6 +1,6 @@
 import { App } from "@octokit/app"
-import setupwebhooks from "@/github/webhooks"
 import env from "@/env"
+import setupwebhooks from "@/github/webhooks"
 
 const githubapp = new App({
 	appId: env.GITHUB_APP_ID,
