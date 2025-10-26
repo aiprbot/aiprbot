2eb225aa0c535b02513deb0ab905556e38e7ad62
sam <aquaticcalf@proton.me>
2025-10-26 15:30:53 +0530

    aipr

---
+++ app/index.ts
@@ -0,0 +1,7 @@
+import { Elysia } from "elysia"
+
+const app = new Elysia().get("/", () => "hello elysia").listen(4321)
+
+console.log(
+	`🦊 elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+)
